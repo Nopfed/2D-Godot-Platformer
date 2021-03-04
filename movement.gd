bb8654461bd8028560865c707d99914955a014df
Rob Toth <robby.tsunami@gmail.com>
2021-03-03 20:56:23 -0700

    project folder update

--- movement.gd
+++ movement.gd
@@ -2,18 +2,25 @@ extends KinematicBody2D
 
 #velocity
 var v = Vector2(0, 0)
+const speedIncrement = 30
+const speedMax = 300
 
 func _physics_process(delta):
+	#move to the right
 	if Input.is_action_pressed("right"):
 		if !Input.is_action_pressed("left"):
-			v.x += 30 if v.x < 300 else 0
+			v.x += speedIncrement if v.x < speedMax else 0
 	
+	#move to the left
 	if Input.is_action_pressed("left"):
 		if !Input.is_action_pressed("right"):
-			v.x -= 30 if v.x > -300 else 0
+			v.x -= speedIncrement if v.x > -speedMax else 0
+	
+	v.y = v.y + 30
 	
 	move_and_slide(v)
 	
+	#inertia/friction
 	if !Input.is_action_pressed("right"):
 		if !Input.is_action_pressed("left"):
 			v.x = lerp(v.x, 0, 0.9)
