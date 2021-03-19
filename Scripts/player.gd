5b43a6c4c8b5159fee5248c7c07a1fa5a0526ec3
Rob Toth <robby.tsunami@gmail.com>
2021-03-19 14:07:05 -0600

    wall jumping

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,6 +5,8 @@ var v = Vector2(0, 0)
 var collectibles = 0
 var playing = true
 var jumping = false
+var walljumpingRight = false
+var walljumpingLeft = false
 onready var floortype = "normal"
 
 const SPEED = 300
@@ -14,7 +16,7 @@ const JUMP = -615
 
 func _physics_process(_delta):
 	#move to the right
-	if Input.is_action_pressed("right"):
+	if Input.is_action_pressed("right") and !walljumpingLeft:
 		if !Input.is_action_pressed("left"):
 			#v.x += SPEED if v.x < MAX_SPEED else 0
 			if floortype == "sticky":
@@ -24,7 +26,7 @@ func _physics_process(_delta):
 	
 	
 	#move to the left
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("left") and !walljumpingRight:
 		if !Input.is_action_pressed("right"):
 			#v.x -= SPEED if v.x > -MAX_SPEED else 0
 			if floortype == "sticky":
@@ -44,11 +46,46 @@ func _physics_process(_delta):
 		jumping = true
 		$"Jump Sound".play()
 	
+	#wall-jumping
+#	if Input.is_action_just_pressed("jump") and is_on_wall() and !is_on_floor() and playing:
+#		floortype = "bouncy"
+#		if Input.is_action_pressed("right") and !Input.is_action_pressed("left"):
+#			walljumpingLeft = true
+#			walljumpingRight = false
+#			v = Vector2(-SPEED*2, JUMP)
+#			jumping = true
+#			$"Jump Sound".play()
+#		elif Input.is_action_pressed("left") and !Input.is_action_pressed("right"):
+#			walljumpingRight = true
+#			walljumpingLeft = false
+#			v = Vector2(SPEED*2, JUMP)
+#			jumping = true
+#			$"Jump Sound".play()
+			
+	if is_on_wall() and !is_on_floor() and Input.is_action_pressed("left") and !Input.is_action_pressed("right") and Input.is_action_just_pressed("jump"):
+		floortype = "bouncy"
+		walljumpingRight = true
+		walljumpingLeft = false
+		v = Vector2(SPEED*2, JUMP*1.1)
+		jumping = true
+		$"Jump Sound".play()
+	elif is_on_wall() and !is_on_floor() and Input.is_action_pressed("right") and !Input.is_action_pressed("left") and Input.is_action_just_pressed("jump"):
+		floortype = "bouncy"
+		walljumpingLeft = true
+		walljumpingRight = false
+		v = Vector2(-SPEED*2, JUMP*1.1)
+		jumping = true
+		$"Jump Sound".play()
+	
 	if Input.is_action_just_released("jump") and v.y < -100 and jumping:
 		v.y = -100
 	
 	if jumping and v.y >= 0:
 		jumping = false
+	
+	if (walljumpingRight or walljumpingLeft) and is_on_floor():
+		walljumpingRight = false
+		walljumpingLeft = false
 	
 	var snap = Vector2.DOWN * 32 if !jumping else Vector2.ZERO
 	
