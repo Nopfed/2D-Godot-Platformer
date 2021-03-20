3030b228199e01440f1973946764df111e18ceed
Rob Toth <robby.tsunami@gmail.com>
2021-03-19 18:16:50 -0600

    0.4.0 release

--- Scripts/win zone 0.gd
+++ Scripts/win zone 0.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 
-func _on_win_zone_body_entered(body):
+func _on_win_zone_body_entered(_body):
 	$"win sound".play()
 	yield($"win sound", "finished")
 	get_tree().change_scene("res://Scenes/levels/level 1.tscn")
